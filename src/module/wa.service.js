8e8d32ad8fa407196be1d5dfa21e32964db4f16a
Viper <piusrestiantoro2@gmail.com>
2024-05-29 11:00:51 +0800

    fix: crashed handler

--- src/module/wa.service.js
+++ src/module/wa.service.js
@@ -3,6 +3,8 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const logger = require('../utils/logger');
 
+let waClient = null;
+
 const checkConnection = async () => {
     try {
         await axios.get('https://www.google.com');
@@ -13,41 +15,58 @@ const checkConnection = async () => {
 };
 
 const initializeClient = async () => {
-    const connectionStatus = await checkConnection();
-    if (!connectionStatus) {
-        logger.error('No internet connection. Please check your network');
+    const connectionChecker = async () => {
+        const connectionStatus = await checkConnection();
+        if (!connectionStatus) {
+            logger.error('No internet connection. Please check your network');
+        }
+    };
+
+    if (!waClient) {
+        try {
+            waClient = new Client({
+                authStrategy: new LocalAuth(),
+                puppeteer: {
+                    headless: true,
+                    args: ['--no-sandbox', '--disable-gpu'],
+                },
+                webVersionCache: {
+                    type: 'remote',
+                    remotePath:
+                        'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+                },
+            });
+
+            waClient.on('qr', (qr) => qrcode.generate(qr, { small: true }));
+            waClient.on('ready', () => logger.info('Client is ready!'));
+            waClient.on('message', async (msg) => {
+                try {
+                    if (msg.from !== 'status@broadcast') {
+                        const contact = await msg.getContact();
+                        console.log(contact, msg.body);
+                        // logger.log(contact, msg.body);
+                    }
+                } catch (error) {
+                    logger.error(error.message);
+                }
+            });
+
+            waClient.on('disconnected', (reason) => {
+                logger.error(`Client disconnected: ${reason}. Reconnecting...`);
+                connectionChecker();
+            });
+
+            logger.info('waClient initialized successfully');
+        } catch (error) {
+            logger.error(error.message);
+        }
     }
 };
 
-const waClient = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        headless: true,
-        args: ['--no-sandbox', '--disable-gpu'],
-    },
-    webVersionCache: {
-        type: 'remote',
-        remotePath:
-            'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-    },
-});
+initializeClient();
 
-waClient.on('qr', (qr) => qrcode.generate(qr, { small: true }));
-waClient.on('ready', () => logger.info('Client is ready!'));
-waClient.on('message', async (msg) => {
-    try {
-        if (msg.from !== 'status@broadcast') {
-            const contact = await msg.getContact();
-            console.log(contact, msg.body);
-            // logger.log(contact, msg.body);
-        }
-    } catch (error) {
-        logger.error(error.message);
-    }
-});
-
-waClient.on('disconnected', (reason) => {
-    logger.error(`Client disconnected: ${reason}. Reconnecting...`);
+process.on('uncaughtException', (err) => {
+    logger.error(err);
     initializeClient();
 });
 
